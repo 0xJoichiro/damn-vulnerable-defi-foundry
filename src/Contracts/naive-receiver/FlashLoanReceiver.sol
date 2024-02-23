c5cdabed7f292bb85d26f47171ad68b0a2638d54
0xJoichiro <joichiro.sai@gmail.com>
2024-02-23 22:29:48 +0530

    major contracts added

--- src/Contracts/naive-receiver/FlashLoanReceiver.sol
+++ src/Contracts/naive-receiver/FlashLoanReceiver.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import "solady/src/utils/SafeTransferLib.sol";
+import "solady/utils/SafeTransferLib.sol";
 import "openzeppelin-contracts/contracts/interfaces/IERC3156FlashBorrower.sol";
 import "./NaiveReceiverLenderPool.sol";
 
